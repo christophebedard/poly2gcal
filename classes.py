c49613680decf3064967355d35928a864227f742
Christophe Bedard <bedard.christophe@gmail.com>
2019-01-12 15:03:53 -0500

    Add basic classes to represent courses

---
+++ classes.py
@@ -0,0 +1,69 @@
+## Classes used for time conversion/processing
+
+from datetime import datetime, timedelta
+
+
+class TimeStringUtils:
+    TIME_FORMAT = '%d/%b/%Y'
+    WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    WEEKDAYS_INT = {}
+    for i, weekday in enumerate(WEEKDAYS):
+        WEEKDAYS_INT[weekday] = i
+
+    def to_datetime(time_str):
+        return datetime.strptime(time_str, TimeStringUtils.TIME_FORMAT)
+    
+    def weekday(weekday_str):
+        return TimeStringUtils.WEEKDAYS_INT[weekday_str]
+
+
+class SemesterInfo:
+
+    def __init__(self, firstweek_day, lastweek_day, last_day, breakweek_day):
+        self.firstweek_day: datetime = TimeStringUtils.to_datetime(firstweek_day)
+        self.lastweek_day: datetime = TimeStringUtils.to_datetime(lastweek_day)
+        self.last_day: datetime = TimeStringUtils.to_datetime(last_day)
+        self.breakweek_day: datetime = TimeStringUtils.to_datetime(breakweek_day)
+
+
+class Timeslot:
+
+    def __init__(self, day, start, duration, room):
+        self.start: timedelta = Timeslot.start_timedelta(day, start)
+        self.duration: timedelta = Timeslot.duration_timedelta(duration)
+        self.room: str = room
+
+    def start_timedelta(day, start):
+        start_time = datetime.strptime(start, '%H%M')
+        return timedelta(days=TimeStringUtils.weekday(day), hours=start_time.hour, minutes=start_time.minute)
+    
+    def duration_timedelta(duration):
+        total_minutes = (60 * duration) - 10
+        hours = int(total_minutes / 60)
+        minutes = total_minutes % 60
+        return timedelta(hours=hours, minutes=minutes)
+
+
+class AltTimeslot(Timeslot):
+
+    def __init__(self, day, start, duration, room, week):
+        super().__init__(day, start, duration, room)
+        self.week: str = week
+
+
+class Course:
+
+    def __init__(self, name, cid, lectures, lab):
+        self.name: str = name
+        self.cid: str = cid
+        self.lectures: list = lectures
+        self.lab: AltTimeslot | Timeslot  = lab
+
+
+class Courses:
+
+    def __init__(self):
+        self.courses: list = []
+    
+    def add(self, course):
+        self.courses.append(course)
---
+++ input.py
@@ -0,0 +1,84 @@
+## Input data
+
+from datetime import datetime, timedelta
+from classes import TimeStringUtils, SemesterInfo, Timeslot, AltTimeslot, Course, Courses
+
+semester_info = SemesterInfo(firstweek_day='7/Jan/2019',
+                             lastweek_day='29/Apr/2019',
+                             last_day='4/May/2019',
+                             breakweek_day='4/Mar/2019')
+
+courses = Courses()
+
+inf3610 = Course(name='Systemes embarques',
+                 cid='INF3610',
+                 lectures=[Timeslot(day='Monday',
+                                    start='0830',
+                                    duration=3,
+                                    room='M-2203')],
+                 lab=AltTimeslot(day='Friday',
+                                 start='0830',
+                                 duration=3,
+                                 room='L-3712',
+                                 week='B1'))
+
+inf4420a = Course(name='Securite informatique',
+                  cid='INF4420A',
+                  lectures=[Timeslot(day='Wednesday',
+                                     start='1445',
+                                     duration=3,
+                                     room='M-1510')],
+                  lab=AltTimeslot(day='Thursday',
+                                  start='1345',
+                                  duration=3,
+                                  room='L-4712',
+                                  week='B1'))
+
+inf8480 = Course(name='Syst repartis et infonuagique',
+                 cid='INF8480',
+                 lectures=[Timeslot(day='Friday',
+                                    start='0830',
+                                    duration=3,
+                                    room='M-1010')],
+                 lab=AltTimeslot(day='Tuesday',
+                                 start='1545',
+                                 duration=3,
+                                 room='L-4712',
+                                 week='B1'))
+
+inf8770 = Course(name='Technologies multimedias',
+                 cid='INF8770',
+                 lectures=[Timeslot(day='Monday',
+                                    start='0830',
+                                    duration=3,
+                                    room='M-1420')],
+                 lab=AltTimeslot(day='Thursday',
+                                 start='1445',
+                                 duration=3,
+                                 room='L-3712',
+                                 week='B2'))
+
+phs1102 = Course(name='Champs electromagnetiques',
+                 cid='PHS1102',
+                 lectures=[Timeslot(day='Monday',
+                                    start='1130',
+                                    duration=1,
+                                    room='B-316.1'),
+                           Timeslot(day='Wednesday',
+                                    start='1245',
+                                    duration=2,
+                                    room='B-316.1')],
+                 lab=Timeslot(day='Friday',
+                              start='1245',
+                              duration=3,
+                              room='C-539.6'))
+
+courses.add(inf3610)
+courses.add(inf4420a)
+courses.add(inf8480)
+courses.add(inf8770)
+courses.add(phs1102)
+
+# tests
+print(semester_info.firstweek_day + inf4420a.lectures[0].start)
+print(TimeStringUtils.weekday('Friday'))
