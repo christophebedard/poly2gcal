98deb26aaa73b17f7518903b69904171a84c179e
Christophe Bedard <bedard.christophe@gmail.com>
2019-01-16 18:02:33 -0500

    Extract week interval format method

--- weekdates_intervals.py
+++ weekdates_intervals.py
@@ -9,6 +9,16 @@ from datetime import datetime, timedelta
 from time_tools import month_from_date
 from conversion_tools import convert_semester_info, convert_courses
 
+def get_interval(date_begin, date_end):
+    """
+    Format an interval (begin and end dates) to string 
+    """
+    begin_month = month_from_date(date_begin)
+    end_month = month_from_date(date_end)
+    return 'Week of {} {} to{} {}:'.format(begin_month,
+                                           date_begin.day,
+                                           ' ' + end_month if begin_month != end_month else '',
+                                           date_end.day)
 
 def main():
     with open('input_data.json') as f:
@@ -20,15 +30,7 @@ def main():
     first_day = semester_info['firstweek_day']
     while first_day <= semester_info['lastweek_day']:
         last_day = first_day + timedelta(days=6)
-        
-        begin_month = month_from_date(first_day)
-        end_month = month_from_date(last_day)
-
-        print('Week of {} {} to{} {}:'.format(begin_month,
-                                              first_day.day,
-                                              ' ' + end_month if begin_month != end_month else '',
-                                              last_day.day))
-
+        print(get_interval(first_day, last_day))
         first_day += timedelta(weeks=1)
 
 if __name__ == '__main__':
