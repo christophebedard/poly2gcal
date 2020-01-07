335960f42a832a28aa51c30aef840a578f84bb39
Christophe Bedard <bedard.christophe@gmail.com>
2020-01-06 20:13:29 -0500

    Print args' default values with help and add longer option

--- poly2gcal/args.py
+++ poly2gcal/args.py
@@ -7,19 +7,21 @@ def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='')
     parser.add_argument(
         '-t',
+        '--test',
         dest='test',
         action='store_const',
         const=True,
         default=False,
-        help='test: only preview requests & do not actually add to calendar',
+        help='only preview requests & do not actually add to calendar (default: %(default)s)',
     )
     parser.add_argument(
         '-c',
+        '--checklist',
         dest='checklist',
         action='store_const',
         const=True,
         default=False,
-        help='generate checklist of labs to hand in, separated by weeks',
+        help='generate checklist of labs to hand in, separated by weeks (default: %(default)s)',
     )
     parser.add_argument(
         '-f',
